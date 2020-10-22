8b6a5a708609cc162906d8c82080c98aaf8181c2
Murrkoxx <51002238+Murrkoxx@users.noreply.github.com>
2020-10-22 20:30:01 +0300

    Create new.php

---
+++ new.php
@@ -0,0 +1,13 @@
+<html>
+<body><head><title>tESTING 123
+</title></head>
+<?php
+ $a=25; //numerical values
+ $b ="Hello"; //string variables
+ $c = 5.7; //float variable
+ echo "Number is :".$a."</br>";
+echo "String is :">$b."</br>";
+echo "float value:".$c;
+?>
+</body>
+</html>
